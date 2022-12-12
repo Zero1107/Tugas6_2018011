c676b87303816677be6b6aeaae35f94b5fdb97ca
Zero1107 <fiqihdiouf70@gmail.com>
2022-12-12 22:07:41 +0700

    Rest API

--- app/src/main/java/com/example/hirarth_kitchen/Home.java
+++ app/src/main/java/com/example/hirarth_kitchen/Home.java
@@ -1,77 +1,31 @@
 package com.example.hirarth_kitchen;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
-import com.google.android.material.navigation.NavigationView;
+import com.example.hirarth_kitchen.databinding.ActivityMainBinding;
 
 public class Home extends AppCompatActivity {
-    private DrawerLayout dl;
-    private ActionBarDrawerToggle abdt;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        dl = (DrawerLayout)findViewById(R.id.dl);
-        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
-        abdt.setDrawerIndicatorEnabled(true);
-
-        dl.addDrawerListener(abdt);
-        abdt.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        final OneTimeWorkRequest request = new OneTimeWorkRequest.Builder(MyWorker.class).build();
+        binding.ButtonOrder.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if (id == R.id.nav_home) {
-                    Intent a = new Intent(Home.this,
-                            Home.class);
-                    startActivity(a);
-                } else if (id == R.id.nav_profile) {
-                    Intent a = new Intent(Home.this,
-                            Profile.class);
-                    startActivity(a);
-                } else if (id == R.id.nav_chat) {
-                    Intent a = new Intent(Home.this,
-                            Chat.class);
-                    startActivity(a);
-                } else if (id == R.id.nav_setting) {
-                    Intent a = new Intent(Home.this,
-                            Setting.class);
-                    startActivity(a);
-                }else if (id == R.id.nav_alarm) {
-                    Intent a = new Intent(Home.this,
-                            MainActivity.class);
-                    startActivity(a);
-                }
-                dl.closeDrawer(GravityCompat.START);
-                return true;
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueueUniqueWork(
+                        "Notifikasi", ExistingWorkPolicy.REPLACE, request);
             }
         });
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return abdt.onOptionsItemSelected(item) ||
-                super.onOptionsItemSelected(item);
-    }
-    //on back press behavior
-    @Override
-    public void onBackPressed() {
-        if (dl.isDrawerOpen(GravityCompat.START)) {
-            dl.closeDrawer(GravityCompat.START);
-        }
-        else {
-            super.onBackPressed();
-        }
     }
 }
